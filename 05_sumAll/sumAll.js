2607833e9c6f3b57943de290d91fea4ae43662c9
Redakas <andregabrielmelo@gmail.com>
2025-02-25 12:25:43 -0300

    Add solution to 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,30 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+    if (
+        start < 0 ||
+        end < 0 ||
+        start != parseInt(start, 10) ||
+        end != parseInt(end, 10) ||
+        typeof start != "number" ||
+        typeof end != "number"
+    )
+        return "ERROR";
 
+    let sum = 0;
+
+    while (start != end) {
+        sum += start;
+        if (start < end) {
+            start++;
+        } else {
+            start--;
+        }
+    }
+    sum += end;
+
+    return sum;
 };
+
+console.log(sumAll(2.5, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
