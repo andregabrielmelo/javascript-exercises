569c3ca6d27db12f9089e8e4034f765876d3dafd
Redakas <andregabrielmelo@gmail.com>
2025-02-25 11:59:12 -0300

    Add solution to 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...elementsToRemove) {
+    return array.filter((element) => {
+        for (const elementToRemove of elementsToRemove) {
+            if (element === elementToRemove) return false;
+        }
+        return true;
+    });
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3, 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
