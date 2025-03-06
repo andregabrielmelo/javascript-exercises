b4f2342447460ca6d6eca982da52745ace7d684b
Redakas <andregabrielmelo@gmail.com>
2025-03-05 23:14:03 -0300

    Add problem javascript-exercises/12_findTheOldest solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,37 @@
-const findTheOldest = function() {
+const findTheOldest = (array) =>
+    array
+        .sort((pearson, nextPearson) => {
+            currentYear = new Date().getFullYear();
+            pearsonYearsOld =
+                (pearson["yearOfDeath"] ?? currentYear) -
+                pearson["yearOfBirth"];
+            nextPearsonYearsOld =
+                (nextPearson["yearOfDeath"] ?? currentYear) -
+                nextPearson["yearOfBirth"];
 
-};
+            return pearsonYearsOld > nextPearsonYearsOld ? 1 : -1;
+        })
+        .pop();
+
+console.log(
+    findTheOldest([
+        {
+            name: "Carly",
+            yearOfBirth: 1942,
+            yearOfDeath: 1970,
+        },
+        {
+            name: "Ray",
+            yearOfBirth: 1962,
+            yearOfDeath: 2011,
+        },
+        {
+            name: "Jane",
+            yearOfBirth: 1912,
+            yearOfDeath: 1941,
+        },
+    ])
+);
 
 // Do not edit below this line
 module.exports = findTheOldest;
