46a5d5e050055d13b6ebe9240b92143fdb67873f
Redakas <andregabrielmelo@gmail.com>
2025-03-05 22:52:32 -0300

    Add problem javascript-exercises/10_fibonacci solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (position) {
+    if (position < 0) return "OOPS";
+    if (position == 0) return 0;
 
+    let previousNumber = 1,
+        beforePreviousNumber = 1;
+    for (let i = 2; i < position; i++) {
+        if (beforePreviousNumber < previousNumber) {
+            beforePreviousNumber = beforePreviousNumber + previousNumber;
+        } else {
+            previousNumber = previousNumber + beforePreviousNumber;
+        }
+    }
+    return Math.max(previousNumber, beforePreviousNumber);
 };
+
+console.log(fibonacci(3));
 
 // Do not edit below this line
 module.exports = fibonacci;
