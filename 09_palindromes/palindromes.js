1991ba8b53d5b029bfb5340dfc56068c565a2869
Redakas <andregabrielmelo@gmail.com>
2025-03-05 22:36:25 -0300

    Add problem javascript-exercises/09_palindromes solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
-
-};
+const palindromes = (str) =>
+    str
+        .toLowerCase()
+        .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+        .replace(/ /g, "") ===
+    str
+        .toLowerCase()
+        .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+        .replace(/ /g, "")
+        .split("")
+        .reverse()
+        .join("");
 
 // Do not edit below this line
 module.exports = palindromes;
